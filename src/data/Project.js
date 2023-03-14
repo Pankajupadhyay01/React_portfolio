64d3e0bfeec632330978b4b14d1b59fc8c9a640b
pankajupadhyay01 <pankajup.git01@gmail.com>
2023-03-14 22:08:15 +0530

    Performance optimized

--- src/data/Project.js
+++ src/data/Project.js
@@ -2,32 +2,32 @@ export const all = [
     { 
         title:"Movie Review Website",
         link:"https://reactmovieapp-pankaj.netlify.app/",
-        img:"./src/assets/movie.png"
+        img:"/src/assets/movie.png"
     },
     { 
         title:"Portfolio Website",
-        img:"./src/assets/react.png"
+        img:"/src/assets/react.png"
     },
     { 
         title:"Responsive Web Section",
         link:"https://codepen.io/Pankaj-Upadhyay-/pens/",
-        img:"./src/assets/hurst.png"
+        img:"/src/assets/hurst.png"
     },
     // html
     { 
         title:"html Portfolio",
         link:"https://pankajupadhyay01.github.io/portfolio/",
-        img:"./src/assets/html.png"
+        img:"/src/assets/html.png"
     },
     // js
     { 
         title:"Text Utilizer",
-        img:"./src/assets/text.png"
+        img:"/src/assets/text.png"
     },
     { 
         title:"Weather App",
         link:"https://wetherjsapi.netlify.app",
-        img:"./src/assets/weather.png"
+        img:"/src/assets/weather.png"
     },
    
 ]
@@ -36,11 +36,11 @@ export const react = [
     { 
         title:"Movie Review Website",
         link:"https://reactmovieapp-pankaj.netlify.app/",
-        img:"./src/assets/movie.png"
+        img:"/src/assets/movie.png"
     },
     { 
         title:"React Portfolio",
-        img:"./src/assets/react.png"
+        img:"/src/assets/react.png"
     }
   
 ]
@@ -50,18 +50,18 @@ export const html = [
     { 
         title:"html Portfolio",
         link:"https://pankajupadhyay01.github.io/portfolio/",
-        img:"./src/assets/html.png"
+        img:"/src/assets/html.png"
     },
     { 
         title:"HTML Section",
         link:"https://codepen.io/Pankaj-Upadhyay-/pens/",
-        img:"./src/assets/hurst.png"
+        img:"/src/assets/hurst.png"
     },
     
     { 
         title:"Source Code Web",
         link:"https://pankajupadhyay01.github.io/cheetport/cheet-sheet.html",
-        img:"./src/assets/cp.png"
+        img:"/src/assets/cp.png"
     },
 ]
 
@@ -70,23 +70,23 @@ export const js = [
     // js
     { 
         title:"Password Genrator",
-        img:"./src/assets/pna.jpg"
+        img:"/src/assets/pna.jpg"
     },
     { 
         title:"Random Joke ",
-        img:"./src/assets/pna.jpg"
+        img:"/src/assets/pna.jpg"
     },
     { 
         title:"Text Utilizer",
-        img:"./src/assets/text.png"
+        img:"/src/assets/text.png"
     },
     { 
         title:"Count Down",
-        img:"./src/assets/cdown.png"
+        img:"/src/assets/cdown.png"
     },
     { 
         title:"Weather App",
         link:"https://wetherjsapi.netlify.app",
-        img:"./src/assets/weather.png"
+        img:"/src/assets/weather.png"
     },
 ]
