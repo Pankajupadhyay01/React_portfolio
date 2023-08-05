e9ae7a47633df78e9a86f3d4557f4b92c7d101b2
pankaj upadhyay <pankajup.git01@gmail.com>
2023-08-05 22:49:27 +0530

    Image changes

--- src/Component/Portfolio.jsx
+++ src/Component/Portfolio.jsx
@@ -1,23 +1,23 @@
 import { React, Suspense, lazy } from 'react';
 import Navbar from './Navbar';
 import Hero from '../Pages/Hero';
-import About from '../Pages/About'; 
+import About from '../Pages/About';
 import Contact from '../Pages/Contact';
 import Skill from '../Pages/Skill';
 import Projects from '../Pages/Projects'
-import Footer from '../Component/Footer'; 
+import Footer from '../Component/Footer';
 
 
 const Portfolio = () => {
   return (
     <div>
       <Navbar />
-      <Hero /> 
-        <About />
-        <Skill />
-        <Projects />
-        <Contact />
-        <Footer /> 
+      <Hero />
+      <About />
+      <Skill />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   );
 }
