89b98460ff49252378b9e26b707f506396b2816a
pankaj upadhyay <pankajup.git01@gmail.com>
2023-08-10 00:26:43 +0530

    project card updated

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <Heading name={"About Me"} />
       <div className='flex flex-col-reverse lg:flex-row m-[40px_auto] md:w-[95%] w-[85%] lg:w-[92%] justify-between'>
         <div className='lg:w-[45%] flex  justify-center'>
-          <img className='w-[400px] h-[400px] rounded-3xl object-cover' src="assets/img6.jpeg" width={180} height={180} loading='lazy' alt="My image bg" />
+          <img className='w-[400px] h-[400px] rounded-3xl object-cover' src="assets/img6.jpeg" srcset="/assets/img6.jpeg" width={180} height={180} loading='lazy' alt="My image bg" />
         </div>
         <div className='lg:w-[55%] text-black capitalize font-semibold text-xl text-justify flex flex-wrap m-auto align-middle'>
 
